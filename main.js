df09dfdd59af431d31049f08f08fe6331fa5dc8b
Excalizan <aydinkement24@gmail.com>
2023-08-24 12:37:26 +0300

    torus doesn't work :(

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ import './style.css'
 import * as THREE from 'three'
 
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+)
 
 const renderer = new THREE.WebGLRenderer({
 	canvas: document.querySelector('#bg'),
@@ -12,4 +18,16 @@ renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(30)
 
-renderer.render(scene, camera)
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 })
+const torus = new THREE.Mesh(geometry, material)
+
+scene.add(torus)
+
+function animate() {
+	requestAnimationFrame(animate)
+
+	renderer.render(scene, camera)
+}
+
+animate()
