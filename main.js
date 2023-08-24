4afbc57f8564fa5faf009f351fc42f7325f10a3a
Excalizan <aydinkement24@gmail.com>
2023-08-24 12:45:36 +0300

    finally works, we can move on now

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import './style.css'
 import * as THREE from 'three'
 
 const scene = new THREE.Scene()
-
 const camera = new THREE.PerspectiveCamera(
 	75,
 	window.innerWidth / window.innerHeight,
@@ -10,18 +9,20 @@ const camera = new THREE.PerspectiveCamera(
 	1000
 )
 
-const renderer = new THREE.WebGLRenderer({
-	canvas: document.querySelector('#bg'),
-})
+const renderer = new THREE.WebGLRenderer()
 
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
 camera.position.setZ(30)
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 })
+const material = new THREE.MeshBasicMaterial({
+	color: 0xff6347,
+	wireframe: true,
+})
 const torus = new THREE.Mesh(geometry, material)
-
 scene.add(torus)
 
 function animate() {
