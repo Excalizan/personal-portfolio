7d3190bd4ef8aeeefb271db64d13d6cddb4174ce
Excalizan <aydinkement24@gmail.com>
2023-08-25 16:59:21 +0300

    add stars

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -22,6 +23,8 @@ const material = new THREE.MeshStandardMaterial({ color: 0xff6347 })
 const torus = new THREE.Mesh(geometry, material)
 scene.add(torus)
 
+const controls = new OrbitControls(camera, renderer.domElement)
+
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(10, 10, 10)
 
@@ -32,12 +35,31 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 const grigHelper = new THREE.GridHelper(200, 50)
 scene.add(lightHelper, grigHelper)
 
+function addstar() {
+	const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+	const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+	const star = new THREE.Mesh(geometry, material)
+
+	const [x, y, z] = Array(3)
+		.fill()
+		.map(() => THREE.MathUtils.randFloatSpread(100))
+
+	star.position.set(x, y, z)
+	scene.add(star)
+}
+
+Array(200).fill().forEach(addstar)
+
+// const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+
 function animate() {
 	requestAnimationFrame(animate)
 
 	torus.rotation.x += 0.01
 	torus.rotation.y += 0.005
 	torus.rotation.z += 0.01
+
+	controls.update()
 
 	renderer.render(scene, camera)
 }
