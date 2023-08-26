e4674bde5d004b746cc42725c896c8a66436855f
Excalizan <aydinkement24@gmail.com>
2023-08-26 14:46:27 +0300

    comments and formatting

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 
+// init
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
 	75,
@@ -8,13 +9,13 @@ const camera = new THREE.PerspectiveCamera(
 	0.1,
 	1000
 )
-
 const renderer = new THREE.WebGLRenderer()
 
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+// torus
 const torusKnotGeometry = new THREE.TorusKnotGeometry(7, 2, 100, 16)
 const torusKnotMaterial = new THREE.MeshNormalMaterial({
 	wireframe: true,
@@ -26,6 +27,7 @@ torusKnot.position.x = 0
 torusKnot.position.y = 0
 torusKnot.position.z = 0
 
+// cone
 const coneGeometry = new THREE.ConeGeometry(5, 10, 32, 16, false, 0, 6.3)
 const coneMaterial = new THREE.MeshBasicMaterial({
 	color: 0xffff00,
@@ -38,9 +40,11 @@ cone.position.x = 20
 cone.position.y = -10
 cone.position.z = 50
 
+// light
 const ambientLight = new THREE.AmbientLight(0xffffff, 2)
 scene.add(ambientLight)
 
+// stars
 function addStar() {
 	const geometry = new THREE.SphereGeometry(0.5, 24, 24)
 	const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
@@ -56,11 +60,11 @@ function addStar() {
 
 Array(2000).fill().forEach(addStar)
 
+// earth
 const earthTexture = new THREE.TextureLoader().load('img/2k_earth_nightmap.jpg')
 const earthNormal = new THREE.TextureLoader().load(
 	'img/2k_earth_normal_map.tif'
 )
-
 const earth = new THREE.Mesh(
 	new THREE.SphereGeometry(3, 32, 32),
 	new THREE.MeshStandardMaterial({
@@ -74,6 +78,7 @@ scene.add(earth)
 earth.position.x = -40
 earth.position.x = -15
 
+// camera movement on scroll
 function moveCamera() {
 	const t = document.body.getBoundingClientRect().top
 	earth.rotation.x += 0.05
@@ -85,6 +90,7 @@ function moveCamera() {
 
 document.body.onscroll = moveCamera
 
+// main loop
 function animate() {
 	requestAnimationFrame(animate)
 
